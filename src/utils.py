cebe48589ca8a4c4c4cefd58c2ca37a825c1ab38
gouravengineer <gouravenginner@gmail.com>
2024-03-07 16:37:06 +0530

    current IST time upto milliseconds

--- src/utils.py
+++ src/utils.py
@@ -8,25 +8,9 @@ def generate_unique_string(length:int=8) -> str:
 
 
 
-# Function to convert UTC to IST
-def ist_datetime_current(utc_datetime_str=None):
-    # If no input is provided, use current UTC time
-    if utc_datetime_str is None:
-        utc_datetime = datetime.utcnow()
-    else:
-        # Parse UTC datetime string to datetime object
-        utc_datetime = datetime.strptime(utc_datetime_str, "%Y-%m-%d %H:%M:%S")
-
-    # Define UTC timezone
-    utc_timezone = pytz.timezone('UTC')
-
-    # Set UTC timezone to the datetime object
-    utc_datetime = utc_timezone.localize(utc_datetime)
-
-    # Define IST timezone
-    ist_timezone = pytz.timezone('Asia/Kolkata')
-
-    # Convert to IST
-    ist_datetime = utc_datetime.astimezone(ist_timezone)
-
-    return ist_datetime
+def ist_datetime_current():
+   """ Get current time of 'Asia/kolkata time zone. """
+   timezone = pytz.timezone('Asia/Kolkata')
+   kolkata_time = datetime.now(timezone)
+   # MySQL DATETIME format with microseconds
+   return kolkata_time.strftime('%Y-%m-%d %H:%M:%S.%f')
